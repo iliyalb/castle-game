aa77a609c3bfce5ae28ffe690211fa3c7191231d
ILIYA LESANI <liverkingson@hotmail.com>
2019-01-15 15:34:50 +0330

    Add projects

---
+++ CastleClient/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+namespace CastleClient
+{
+    public class Client : MonoBehaviour
+    {
+        #region fields
+        private static Socket _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static List<SocketException> _clientSocketError = new List<SocketException>();
+        #endregion
+
+        public void Connect(string ip)
+        {
+            StartCoroutine(AttemptConnect(ip));
+        }
+        public IEnumerator AttemptConnect(string ip)
+        {
+            if (ip != "local")
+            {
+                if (Application.internetReachability != NetworkReachability.NotReachable)
+                {
+                    Debug.Log("Network rechable");
+
+                    int attempts = 0;
+
+                    while (!_clientSocket.Connected)
+                    {
+                        try
+                        {
+                            attempts++;
+                            _clientSocket.Connect(IPAddress.Parse(ip), 100);
+                        }
+                        catch (SocketException)
+                        {
+                            Debug.Log("Connection attempts: " + attempts.ToString());
+                            if (attempts == 2)
+                                Debug.Log("Connection failed after " + attempts + " attempts");
+                        }
+                        yield return new WaitForSeconds(3);
+                    }
+                    Debug.Log("Connected");
+                }
+                else
+                {
+                    Debug.Log("Network Unrechable");
+                }
+            }
+            else
+            {
+                Debug.Log("Loopback");
+
+                int attempts = 0;
+
+                while (!_clientSocket.Connected)
+                {
+                    try
+                    {
+                        attempts++;
+                        _clientSocket.Connect(IPAddress.Loopback, 100);
+                    }
+                    catch (SocketException)
+                    {
+
+                        Debug.Log("Connection attempts: " + attempts.ToString());
+                        if (attempts == 2)
+                            Debug.Log("Connection failed after " + attempts + " attempts");
+                    }
+                    yield return new WaitForSeconds(3);
+                }
+                Debug.Log("Connected");
+            }
+        }
+        public static void Send(string req)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(req);
+
+            try
+            {
+                _clientSocket.Send(buffer);
+            }
+            catch (SocketException se)
+            {
+                Debug.Log("Connection failed");
+                _clientSocketError.Add(se);
+            }
+
+            byte[] receivedBuffer = new byte[1024];
+
+            try
+            {
+                int rec = _clientSocket.Receive(receivedBuffer);
+                byte[] data = new byte[rec];
+                Array.Copy(receivedBuffer, data, rec);
+                Debug.Log("Received: " + Encoding.ASCII.GetString(data));
+            }
+            catch (SocketException se)
+            {
+                _clientSocketError.Add(se);
+            }
+            if (req == "exit")
+                Exit();
+            if (req == "debug")
+            {
+                if (_clientSocketError.Count > 0)
+                    _clientSocketError.ForEach(i => Debug.Log(i + "\n"));
+                else
+                    Debug.Log("No socket errors found");
+            }
+        }
+        public static void Exit()
+        {
+            System.Environment.Exit(0);
+        }
+    }
+}
---
+++ CastleServer/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastleServer
+{
+    [Serializable]
+    class Program
+    {
+        private static readonly Socket _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static readonly List<Socket> _clientSockets = new List<Socket>();
+        private const int BUFFER_SIZE = 8192;
+        private const int PORT = 100;
+        private static byte[] _buffer = new byte[BUFFER_SIZE];
+
+        //public static bool[] _tutorial;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Castle Server";
+            Console.WriteLine(@"_________                  __  .__             _________                                ");
+            Console.WriteLine(@"\_   ___ \_____    _______/  |_|  |   ____    /   _____/ ______________  __ ___________ ");
+            Console.WriteLine(@"/    \  \/\__  \  /  ___/\   __\  | _/ __ \   \_____  \_/ __ \_  __ \  \/ // __ \_  __ \");
+            Console.WriteLine(@"\     \____/ __ \_\___ \  |  | |  |_\  ___/   /        \  ___/|  | \/\   /\  ___/|  | \/");
+            Console.WriteLine(@" \______  (____  /____  > |__| |____/\___  > /_______  /\___  >__|    \_/  \___  >__|   ");
+            Console.WriteLine(@"        \/     \/     \/                 \/          \/     \/                 \/       ");
+            Console.WriteLine();
+            SetupServer();
+            Console.ReadLine();
+            CloseAllSockets();
+
+        }
+
+        private static void SetupServer()
+        {
+            Console.WriteLine(DateTime.Now + " Setting up server...");
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Any, PORT));
+            _serverSocket.Listen(1000);
+            _serverSocket.BeginAccept(AcceptCallback, null);
+            Console.WriteLine(DateTime.Now + " Server up and running.");
+        }
+
+        private static void CloseAllSockets()
+        {
+            foreach (Socket socket in _clientSockets)
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            _serverSocket.Close();
+        }
+
+        private static void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket;
+            try
+            {
+                socket = _serverSocket.EndAccept(AR);
+            }
+            catch (ObjectDisposedException)
+            {
+                return;
+            }
+            _clientSockets.Add(socket);
+            socket.BeginReceive(_buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, socket);
+            Console.WriteLine(DateTime.Now + " Remote: " + socket.RemoteEndPoint.ToString() + " connected. Buffer size: "
+                + socket.ReceiveBufferSize.ToString() + " Timeout: " + socket.ReceiveTimeout.ToString());
+            _serverSocket.BeginAccept(AcceptCallback, null);
+        }
+
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket current = (Socket)AR.AsyncState;
+            int received;
+            try
+            {
+                received = current.EndReceive(AR);
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine(DateTime.Now + " " + current.RemoteEndPoint.ToString() + " Client disconnected");
+                current.Close();
+                _clientSockets.Remove(current);
+                return;
+            }
+            byte[] dataBuffer = new byte[received];
+            Array.Copy(_buffer, dataBuffer, received);
+            string text = Encoding.ASCII.GetString(dataBuffer);
+            Console.WriteLine(DateTime.Now + " " + current.RemoteEndPoint.ToString() + " received: " + text);
+
+            string response = string.Empty;
+
+            text = text.ToLower();
+
+            switch (text)
+            {
+                case "debug":
+                    {
+                        byte[] data = Encoding.ASCII.GetBytes("DEBUG CODE RECEIVED");
+                        current.Send(data);
+                        break;
+                    }
+                case "get time":
+                    {
+                        Console.WriteLine(DateTime.Now + " REQUEST: get time");
+                        byte[] data = Encoding.ASCII.GetBytes(DateTime.Now.ToLongTimeString());
+                        current.Send(data);
+                        Console.WriteLine(DateTime.Now + " REPLY: get time");
+                        break;
+                    }
+                case "exit":
+                    {
+                        current.Shutdown(SocketShutdown.Both);
+                        current.Close();
+                        _clientSockets.Remove(current);
+                        Console.WriteLine(DateTime.Now + " Client disconnected");
+                        return;
+                    }
+                default:
+                    {
+                        Console.WriteLine(DateTime.Now + " Invalid input: " + text);
+                        byte[] data = Encoding.ASCII.GetBytes("Inavlid request");
+                        current.Send(data);
+                        Console.WriteLine(DateTime.Now + " " + current.RemoteEndPoint.ToString() + " Warning sent");
+                        break;
+                    }
+            }
+            current.BeginReceive(_buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, current);
+        }
+    }
+}
